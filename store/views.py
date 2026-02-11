b40120f7f5a586e6112d9a7fa110a309127994ad
Keith Barrington <kbarrington95@gmail.com>
2026-02-11 16:52:21 -0600

    Product & Collection views now use simpler Generic Views

--- store/views.py
+++ store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.db.models.aggregates import Count
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -19,24 +19,35 @@ class ProductList(ListCreateAPIView):
         return {'request': self.request}
     
 
-class ProductDetail(APIView):
-    def get(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializers(product)
-        return Response(serializer.data)
-    
-    def put(self, request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializers(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializers
 
-    def delete(self, request, id):
-         product = get_object_or_404(Product, pk=id)
+    def delete(self, request, pk):
+         product = get_object_or_404(Product, pk=pk)
          if product.orderitem_set.count() > 0: # type: ignore
             return Response({'error':'Cannot delete, This Product is a part of an existing Order'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
          product.delete()
          return Response(status=status.HTTP_204_NO_CONTENT)
+    
+# class ProductDetail(APIView):
+#     def get(self, request, id):
+#         product = get_object_or_404(Product, pk=id)
+#         serializer = ProductSerializers(product)
+#         return Response(serializer.data)
+    
+#     def put(self, request, id):
+#         product = get_object_or_404(Product, pk=id)
+#         serializer = ProductSerializers(product, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+
+#     def delete(self, request, id):
+#          product = get_object_or_404(Product, pk=id)
+#          if product.orderitem_set.count() > 0: # type: ignore
+#             return Response({'error':'Cannot delete, This Product is a part of an existing Order'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#          product.delete()
+#          return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class CollectionList(ListCreateAPIView):
@@ -55,21 +66,14 @@ class CollectionList(ListCreateAPIView):
 #         serializer.save()
 #         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Collection.objects.annotate(products_count=Count('products'))
+    serializer_class = CollectionSerializer
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, pk):
-    collection = get_object_or_404(Collection.objects.annotate(
-        products_count=Count('products')), pk=pk)
-    if request.method == 'GET':
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CollectionSerializer(collection, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
-        if collection.products.count() > 0: # type: ignore
+    def delete(self, request, pk):
+         collection = get_object_or_404(Collection, pk=pk)
+         if collection.products.count() > 0: # type: ignore
             return Response({'error':'Cannot delete, This Collection has one or more Products'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+         collection.delete()
+         return Response(status=status.HTTP_204_NO_CONTENT)
+    
