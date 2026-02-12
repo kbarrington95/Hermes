14bfab8ecf3870ccee6c2ea863a81e968457b612
Keith Barrington <kbarrington95@gmail.com>
2026-02-12 15:57:56 -0600

    cleaned up views file and removed commented code for more complex/manual APIViews

--- store/views.py
+++ store/views.py
@@ -26,26 +26,6 @@ class ProductViewSet(ModelViewSet):
             return Response({'error':'Cannot delete, This Product is a part of an existing Order'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
     
-    
-# class ProductDetail(APIView):
-#     def get(self, request, id):
-#         product = get_object_or_404(Product, pk=id)
-#         serializer = ProductSerializers(product)
-#         return Response(serializer.data)
-    
-#     def put(self, request, id):
-#         product = get_object_or_404(Product, pk=id)
-#         serializer = ProductSerializers(product, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-
-#     def delete(self, request, id):
-#          product = get_object_or_404(Product, pk=id)
-#          if product.orderitem_set.count() > 0: # type: ignore
-#             return Response({'error':'Cannot delete, This Product is a part of an existing Order'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-#          product.delete()
-#          return Response(status=status.HTTP_204_NO_CONTENT)
-
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
@@ -54,18 +34,6 @@ class CollectionViewSet(ModelViewSet):
         if Product.objects.filter(collection_id=kwargs['pk']).count() > 0:
             return Response({'error':'Cannot delete, This Collection has one or more Products'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
-
-# @api_view(['GET', 'POST'])
-# def collection_list(request):
-#     if request.method == 'GET':
-#         queryset = Collection.objects.annotate(products_count=Count('products')).all()
-#         serializer = CollectionSerializer(queryset, many=True)
-#         return Response(serializer.data)
-#     elif request.method == 'POST':
-#         serializer = CollectionSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ReviewViewset(ModelViewSet):
     serializer_class = ReviewSerializer
