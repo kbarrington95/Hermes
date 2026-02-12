90ad044776d3c41f1c55d7f45e8cc5590d06b384
Keith Barrington <kbarrington95@gmail.com>
2026-02-12 14:45:20 -0600

    nested router for reviews

--- store/views.py
+++ store/views.py
@@ -64,5 +64,10 @@ class CollectionViewSet(ModelViewSet):
 #         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class ReviewViewset(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+
+    def get_serializer_context(self):
+        return {'product_id': self.kwargs['product_pk']}
