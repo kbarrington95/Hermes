62905dd5e9aae47c37ed199fffa9174e4261a5c7
Keith Barrington <kbarrington95@gmail.com>
2026-02-12 21:02:36 -0600

    extended user profile which required reseting the database. I think I backedup and restored the core data but we'll see.

--- storefront/settings.py
+++ storefront/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     'playground',
     'debug_toolbar',
     'store',
-    'store_custom',
     'tags',
     'likes',
-    "notetaker"
+    "notetaker",
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -147,3 +147,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False
 }
+
+AUTH_USER_MODEL = 'core.User'
