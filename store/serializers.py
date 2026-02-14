c3266379f0bda7a53a7f7599af9114e8dcc5fcf0
Keith Barrington <kbarrington95@gmail.com>
2026-02-14 12:41:48 -0600

    added cartitems to new order and cart items are deleted upon order creation

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,7 @@
-from rest_framework import serializers
-from store.models import Order, OrderItem, Product, Collection, Review, Cart, CartItem, Customer
 from decimal import Decimal
+from django.db import transaction
+from rest_framework import serializers
+from .models import Order, OrderItem, Product, Collection, Review, Cart, CartItem, Customer
 
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -122,8 +123,24 @@ class CreateOrderSerializer(serializers.Serializer):
     cart_id = serializers.UUIDField()
 
     def save(self, **kwargs):
-        print(self.validated_data['cart_id'])
-        print(self.context['user_id'])
+        with transaction.atomic():
+            cart_id = self.validated_data['cart_id']
+            user_id = self.context['user_id']
 
-        (customer, created) = Customer.objects.get_or_create(user_id=self.context['user_id'])
-        Order.objects.create(customer=customer)
+            (customer, created) = Customer.objects.get_or_create(user_id=user_id)
+            order = Order.objects.create(customer=customer)
+
+            cart_items = CartItem.objects \
+                                .select_related('product') \
+                                .filter(cart_id=cart_id)
+            order_items = [
+                OrderItem(
+                    order=order,
+                    product=item.product,
+                    unit_price=item.product.unit_price,
+                    quantity=item.quantity
+                ) for item in cart_items
+            ]
+            OrderItem.objects.bulk_create(order_items)
+
+            Cart.objects.filter(pk=cart_id).delete()
