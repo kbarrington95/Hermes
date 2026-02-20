698a4031180e95301e61c81b793a479c900c4101
Keith Barrington <kbarrington95@gmail.com>
2026-02-20 16:11:04 -0600

    added smtp4dev email backend which required docker and tested some email templates from django.

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.core.mail import EmailMessage, BadHeaderError
+from templated_mail.mail import BaseEmailMessage
 
 
 def say_hello(request):
+    try:
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Keith'}
+        )
+        message.send(['KeithB@test.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
