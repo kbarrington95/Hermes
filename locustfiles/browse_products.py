69ae25981d4d6060da605c371f62cf6c644c709d
Keith <kbarrington95@gmail.com>
2026-03-04 17:56:22 -0600

    added locus for some testing and also silk to help optomize. tutorial

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -7,18 +7,18 @@ class WebsiteUser(HttpUser):
     @task(2)
     def view_products(self):
         collection_id = randint(2,6)
-        self.client.get(f'store/products/?collection_id={collection_id}', name='store/products')
+        self.client.get(f'/store/products/?collection_id={collection_id}', name='/store/products')
 
     @task(4)
     def view_product(self):
         product_id = randint(1,500)
-        self.client.get(f'/store/products/{product_id}', name='/store/products/:id')
+        self.client.get(f'/store/products/{product_id}/', name='/store/products/:id')
 
     @task(1)
     def add_to_cart(self):
         product_id = randint(1,10)
         self.client.post(f'/store/carts/{self.cart_id}/items/',
-                          name='store/carts/items',
+                          name='/store/carts/items',
                           json={'product_id': product_id, 'quantity': 1})
 
     def on_start(self):
