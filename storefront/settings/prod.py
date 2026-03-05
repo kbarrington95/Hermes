1994b29b414aa46be51d48dd07cebe9386e88d16
Keith <kbarrington95@gmail.com>
2026-03-05 17:05:19 -0600

    provisioned jawsmysql database from heroku used free version

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,8 +1,13 @@
 from .common import *
 import os
+import dj_database_url
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['hermes-prod-c391af873af7.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
