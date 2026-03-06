e2363e927b6c2a675aa3c09c6fe7fffdee24f915
Keith <kbarrington95@gmail.com>
2026-03-06 14:10:56 -0600

    added ngrok to expose my local environment to the web. Will be needed because I'm switching from a polling celery worker to an asyncronous api

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -4,9 +4,13 @@ load_dotenv()
 DEBUG = True
 
 SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
-
 ASSEMBLY_AI_API_KEY = os.getenv('ASSEMBLY_AI_API_KEY')
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+
+# The leading dot is a wildcard for any ngrok subdomain.
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '.ngrok-free.app']
+WEBHOOK_BASE_URL = "https://2d57-107-209-248-174.ngrok-free.app"
+
 
 DATABASES = {
     'default': {
