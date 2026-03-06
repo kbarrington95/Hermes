e9e0f5305f6981a35ea2b6c0430c1703ac72c91a
Keith <kbarrington95@gmail.com>
2026-03-06 11:27:59 -0600

    further s3 fixes

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -38,6 +38,11 @@ EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
 EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
 
 USE_S3 = os.environ['USE_S3']
+# Global S3 Security & Safety Settings
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERIFY = True
+AWS_QUERYSTRING_AUTH = True  # Required for the signed URLs AssemblyAI uses
 
 if USE_S3:
     INSTALLED_APPS += ['storages']
@@ -50,26 +55,15 @@ if USE_S3:
                 "access_key": os.environ.get('AWS_ACCESS_KEY_ID'),
                 "secret_key": os.environ.get('AWS_SECRET_ACCESS_KEY'),
                 "bucket_name": os.environ.get('AWS_STORAGE_BUCKET_NAME'),
-                "region_name": "us-east-2", # Match your Ohio bucket
+                "region_name": "us-east-2",
+                "signature_version": "s3v4",
             },
         },
-        # ADD THIS SECTION TO FIX THE ERROR
         "staticfiles": {
             "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
     }
 
-# 2. AWS Credentials
-# AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_REGION_NAME = 'us-east-2'
-
-# 3. Security & URL Settings
-AWS_S3_SIGNATURE_VERSION = 's3v4'
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_S3_VERIFY = True
 
 # 4. Tell Django to use S3 for Media Files
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
