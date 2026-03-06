efc0e082a2aeafcd0483be5880e76ce6472a9cb7
Keith <kbarrington95@gmail.com>
2026-03-05 19:34:57 -0600

    dockerized app

--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -14,24 +14,27 @@ DATABASES = {
         'NAME': 'storefront3', 
         'USER': 'root',
         'PASSWORD': 'kabkard',
-        'HOST': '127.0.0.1',
-        'PORT': '3308',
+        'HOST': 'mysql',
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
